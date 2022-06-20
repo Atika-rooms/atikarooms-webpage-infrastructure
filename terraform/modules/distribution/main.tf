804151ffa04ba434e327f6b7f9bdbc677eb5c323
Arnau Llamas <arnau.llamas@gmail.com>
2022-06-20 17:48:50 +0000

    Change custom error response to return a 200

--- terraform/modules/distribution/main.tf
+++ terraform/modules/distribution/main.tf
@@ -96,7 +96,7 @@ resource "aws_cloudfront_distribution" "cdn" {
   custom_error_response {
     error_caching_min_ttl = 10
     error_code            = 403
-    response_code         = 403
+    response_code         = 200
     response_page_path    = "/index.html"
   }
 
